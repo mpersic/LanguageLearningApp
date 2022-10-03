7fb70ad576ad5f10ecbdf5bd3cb4676e17990e72
mpersic <matej.persic@live.com>
2022-10-03 21:09:31 +0200

    grammar unit selection

---
+++ LanguageLearningApp/UI/Modules/Grammar/UnitSelection/GrammarUnitSelectionPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LanguageLearningApp.ExamViewModel;
+
+namespace LanguageLearningApp;
+
+[QueryProperty(nameof(Name), nameof(Name))]
+public partial class GrammarUnitSelectionPage : ContentPage
+{
+    #region Fields
+
+    private GrammarUnitSelectionViewModel GrammarUnitSelectionViewModel;
+
+    #endregion Fields
+
+    public GrammarUnitSelectionPage(GrammarUnitSelectionViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = GrammarUnitSelectionViewModel = vm;
+    }
+
+    public string Name
+    {
+        set
+        {
+            LoadExam(value);
+        }
+    }
+
+    //protected override async void OnAppearing()
+    //{
+    //    base.OnAppearing();
+    //    GrammarUnitSelectionViewModel.IsLoading = true;
+    //    var grammarUnits = await GrammarUnitSelectionViewModel._grammarService.GetSelectedUnits();
+    //    var gradedUnits =
+    //        new ObservableCollection<GradedUnit>
+    //        (grammarUnits.Select(
+    //            unit => new GradedUnit(unit)).ToList());
+    //    var sorted = from unit in gradedUnits
+    //                 orderby unit.Name
+    //                 group unit by unit.NameSort into unitGroup
+    //                 select new Grouping<string, GradedUnit>(unitGroup.Key, unitGroup);
+    //    foreach (var list in sorted)
+    //    {
+    //        GrammarViewModel.GroupedUnits
+    //                .Add(new UnitGroup(list.FirstOrDefault().Name, list.ToList()));
+    //    }
+    //    GrammarViewModel.IsLoading = false;
+    //}
+
+    private async void LoadExam(string name)
+    {
+        //try
+        //{
+        //    GrammarUnitSelectionViewModel.IsLoading = true;
+        //    var grammarUnits = await GrammarUnitSelectionViewModel._grammarService.GetSelectedUnits(name);
+        //    var gradedUnits = new ObservableCollection<GradedUnit>(
+        //        grammarUnits.Select(unit => new GradedUnit(unit)).ToList());
+        //    var sorted = from unit in gradedUnits
+        //                 orderby unit.Name
+        //                 group unit by unit.NameSort into unitGroup
+        //                 select new Grouping<string, GradedUnit>(unitGroup.Key, unitGroup);
+        //    foreach (var list in sorted)
+        //    {
+        //        GrammarUnitSelectionViewModel.GroupedGrammarUnits
+        //                .Add(new UnitGroup(list.FirstOrDefault().Name, list.ToList()));
+        //    }
+        //    GrammarUnitSelectionViewModel.IsLoading = false;
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine("Failed to load animal.");
+        //    GrammarUnitSelectionViewModel.IsLoading = false;
+        //}
+    }
+
+}
---
+++ LanguageLearningApp/UI/Modules/Grammar/UnitSelection/GrammarUnitSelectionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LanguageLearningApp.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageLearningApp
+{
+    public partial class GrammarUnitSelectionViewModel : ObservableObject
+    {
+        public readonly IGrammarService _grammarService;
+
+        [ObservableProperty]
+        private bool isLoading;
+
+        public GrammarUnitSelectionViewModel(IGrammarService grammarService)
+        {
+            _grammarService = grammarService;
+        }
+
+        public ObservableCollection<UnitGroup> GroupedGrammarUnits { get; set; } = new ObservableCollection<UnitGroup>();
+
+    }
+}
